ca3a4df3982d589441ff4b2663f1d1def2999b23
Antônio Camargo <apcamargo@users.noreply.github.com>
2025-08-05 18:24:53 -0300

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://apcamargo.github.io/starlight-test/",
+  site: "https://apcamargo.github.io",
   base: "/starlight-test",
   integrations: [
     starlight({
